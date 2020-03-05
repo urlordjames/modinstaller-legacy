4178928bfcf41f5f62985658ebb4542027cdfab6
urlordjames <urlordjames@gmail.com>
2020-03-05 18:10:01 -0500

    added important things I think (idk how java works)

--- client/src/Main.java
+++ client/src/Main.java
@@ -2,10 +2,18 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Main {
+
+    public static final int width = 500;
+    public static final int height = 500;
+
     public static void main(String[] args) {
         Frame f = new Frame( "hello world" );
+        f.setLayout(null);
         f.addWindowListener( new WindowAdapter(){ public void windowClosing( WindowEvent e ){ System.exit( 0 ); } } );
-        f.setSize( 500, 500 );
+        f.setSize( width, height );
+        TextField text = new TextField(3);
+        text.setBounds(250, 250, 100,  24);
+        f.add(text);
         f.setVisible(true);
     }
 }
