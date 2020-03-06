cd9a5c005b3740488cd1d8e01f97ca393373205d
urlordjames <urlordjames@gmail.com>
2020-03-06 12:52:42 -0500

    added downloading zips

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import org.json.*;
+import net.lingala.zip4j.io.inputstream.ZipInputStream;
+import net.lingala.zip4j.model.LocalFileHeader;
 
 public class Main {
 
@@ -24,8 +26,29 @@ public class Main {
         f.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         f.addWindowListener(new WindowAdapter(){ public void windowClosing( WindowEvent e ){ System.exit( 0 );}});
         f.setSize(width, height);
-        addtxt(jsonparse(getdog("http://173.255.230.249/zingot.json"), "name"), f);
+        String packjson = getdog("http://173.255.230.249/zingot.json");
+        addtxt(jsonparse(packjson, "name"), f);
+        download(jsonparse(packjson, "scripts"), "scripts.zip");
         f.setVisible(true);
+    }
+
+    public void unzip(File zipFile) {
+        LocalFileHeader localFileHeader;
+        int readLen;
+        byte[] readBuffer = new byte[4096];
+        try (FileInputStream fileInputStream = new FileInputStream(zipFile)) {
+            ZipInputStream zipinstream = new ZipInputStream(fileInputStream);
+            while ((localFileHeader = zipinstream.getNextEntry()) != null) {
+                File extractedFile = new File(localFileHeader.getFileName());
+                try (OutputStream outputStream = new FileOutputStream(extractedFile)) {
+                    while ((readLen = zipinstream.read(readBuffer)) != -1) {
+                        outputStream.write(readBuffer, 0, readLen);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String jsonparse(String str, String key) {
@@ -61,5 +84,16 @@ public class Main {
             e.printStackTrace();
         }
         return "une error";
+    }
+
+    public static void download(String request, String outname) {
+        try {
+            URL url = new URL(request);
+            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+            FileOutputStream fos = new FileOutputStream(outname);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
