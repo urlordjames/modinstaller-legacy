7a049675549cd56a3a28c3651bbe123916e6e66f
urlordjames <urlordjames@gmail.com>
2020-03-05 19:48:22 -0500

    added getting of pack json

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,58 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.HttpURLConnection;
 
 public class Main {
 
     public static final int width = 500;
     public static final int height = 500;
+    public JLabel text = null;
 
     public static void main(String[] args) {
-        Frame f = new Frame( "hello world" );
-        f.setLayout(null);
-        f.addWindowListener( new WindowAdapter(){ public void windowClosing( WindowEvent e ){ System.exit( 0 ); } } );
-        f.setSize( width, height );
-        TextField text = new TextField(3);
-        text.setBounds(250, 250, 100,  24);
+        Main main = new Main();
+        main.buildwindow();
+    }
+
+    public void buildwindow() {
+        Frame f = new Frame("hello world");
+        f.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        f.addWindowListener(new WindowAdapter(){ public void windowClosing( WindowEvent e ){ System.exit( 0 );}});
+        f.setSize(width, height);
+        text = new JLabel(htmlformat(getdog("http://173.255.230.249/zingot.json")));
         f.add(text);
         f.setVisible(true);
+    }
+
+    public static String htmlformat(String text) {
+        return "<html>" + text.replaceAll("\n", "<br>") + "</html>";
+    }
+
+    public static String getdog(String request) {
+        try {
+            URL url = new URL(request);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("GET");
+            InputStream is = con.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append("\n");
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "une error";
     }
 }
