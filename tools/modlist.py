00faf7b68428b1459b86876770a47c95a5f88095
urlordjames <urlordjames@gmail.com>
2020-07-18 16:16:34 -0400

    made modlist tool better

--- tools/modlist.py
+++ tools/modlist.py
@@ -2,16 +2,13 @@ import hashlib
 import os
 import json
 
-url = "https://jamesvps.tk/mods/jarbin/"
-
 list = {}
 
 for name in os.listdir("mods"):
-    f = open("mods/" + name, "rb")
-    sha = hashlib.sha256()
-    sha.update(f.read())
-    f.close()
-    list.update({sha.hexdigest(): url + name})
+    with open("mods/" + name, "rb") as f:
+        sha = hashlib.sha256()
+        sha.update(f.read())
+    list.update({sha.hexdigest(): f"https://jamesvps.tk/mods/jarbin/{name}"})
 
 f = open("list.json", "w")
 f.write(json.dumps(list))
